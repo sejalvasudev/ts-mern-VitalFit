677bfbdbc24c73053f16e27fe623d78ef58f3477
sejalvasudev <sejal.vasudev@gmail.com>
2024-07-29 18:09:40 +0530

    Fetch-Products

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
 import './index.css'
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
+import axios from 'axios'
 
+axios.defaults.baseURL =
+process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<HomePage />} />
+      <Route path="product/:slug" element={<ProductPage />} />
+      {/* <Route path="dashboard" element={<Dashboard />} /> */}
+      {/* ... etc. */}
+    </Route>
+  )
+)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,24 +1,81 @@
-import { Col, Row, Card, Button } from 'react-bootstrap'
+import axios from 'axios'
+import { useEffect, useReducer } from 'react'
+import { Col, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import { sampleProducts } from '../data'
+import LoadingBox from '../components/LoadingBox'
+import MessageBox from '../components/MessageBox'
+import { ApiError } from '../types/ApiError'
+import { Product } from '../types/Product'
+import { getError } from '../utils'
+
+type State = {
+  products: Product[]
+  loading: boolean
+  error: string
+}
+
+type Action =
+  | { type: 'FETCH_REQUEST' }
+  | {
+      type: 'FETCH_SUCCESS'
+      payload: Product[]
+    }
+  | { type: 'FETCH_FAIL'; payload: string }
+
+const initialState: State = {
+  products: [],
+  loading: true,
+  error: '',
+}
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true }
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false }
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
 
 export default function HomePage() {
-  return (
+  const [{ loading, error, products }, dispatch] = useReducer<
+    React.Reducer<State, Action>
+  >(reducer, initialState)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' })
+      try {
+        const result = await axios.get('/api/products')
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err as ApiError) })
+      }
+    }
+    fetchData()
+  }, [])
+
+  return loading ? (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant="danger">{error}</MessageBox>
+  ) : (
     <Row>
-      {sampleProducts.map((product) => (
-        <Col key={product.slug} sm={6} md={4} lg={3} className="mb-4">
-          <Card className="h-100">
-            <Link to={'/product/' + product.slug}>
-              <Card.Img variant="top" src={product.image} alt={product.name} />
-            </Link>
-            <Card.Body>
-              <Card.Title>{product.name}</Card.Title>
-              <Card.Text>Rs.{product.price}</Card.Text>
-              <Link to={'/product/' + product.slug}>
-                <Button variant="primary">View Product</Button>
-              </Link>
-            </Card.Body>
-          </Card>
+      {products.map((product) => (
+        <Col key={product.slug} sm={6} md={4} lg={3}>
+          <Link to={'/product/' + product.slug}>
+            <img
+              src={product.image}
+              alt={product.name}
+              className="product-image"
+            />
+            <h2>{product.name}</h2>
+            <p>${product.price}</p>
+          </Link>
         </Col>
       ))}
     </Row>
